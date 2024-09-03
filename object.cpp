ae9aff9b721940083ce6a8e4fd7665c233db708f
Vishux777 <vishwas.23@lpu.in>
2024-09-03 19:35:26 +0530

    Cipher Schools

--- object.cpp
+++ object.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
-class Distance(){
+class Distance{
     private:
     int meter;
 
     public:
-    
+    Distance(){
+        this->meter=10;
+
+    }
+}
+int hi(Distance d1){
+    d1.meter+=20;
 }
 int main(){
     
